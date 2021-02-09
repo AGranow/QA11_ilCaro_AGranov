b340c03cc664c99c7835ec5f08b11c1ad81321c0
Avraham Granov <avraham.granow@gmail.com>
2021-02-09 11:32:55 +0100

    //

--- src/test/java/com/ilcarro/qa/test/OpenHomePageTest.java
+++ src/test/java/com/ilcarro/qa/test/OpenHomePageTest.java
@@ -9,6 +9,7 @@ public class OpenHomePageTest extends TestBase {
     @Test
     public void homePagTest() {
         System.out.println("site open");
+       // System.out.println("site open");
 
         System.out.println("FindCarPresent" + app.getCar().ifFindCarFromPresent1());
         app.getCar().isElementPresent2(By.cssSelector(".Main_mainpage__find_your_car__AHLkw form"));
